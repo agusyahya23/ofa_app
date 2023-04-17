0ac9ccd65fed6e92bf4c7f7ca06932616ab03e8d
ahmrh <ahmadrolandi01@gmail.com>
2023-04-17 12:38:34 +0700

    changing landing page

--- ofa_frontend/components/Landing.js
+++ ofa_frontend/components/Landing.js
@@ -3,24 +3,31 @@ import { Container, Row, Col } from 'reactstrap';
 const Landing = () => {
   return (
     <section className="section position-relative">
+    <div className="backdrop backdrop-one" />
+    <div className="backdrop backdrop-two" />
       <Container>
-        <Row className="align-items-center">
-          <Col lg={6}>
-            <div className="pr-lg-5">
+        <Row className="align-items-center h-100">
+          <Col lg={4}>
+
+          {/* <div className="backdrop backdrop-three" /> */}
+            {/* <div className="pr-lg-5">
+              <div className="backdrop"></div>
               <p className="text-uppercase text-primary font-weight-medium f-14 mb-4">Lorem Ipsum</p>
               <h1 className="mb-4 font-weight-normal line-height-1_4">Simply dummy text of the printing <span className="text-primary font-weight-medium">Name</span></h1>
               <p className="text-muted mb-4 pb-2">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
               <a href="#" className="btn btn-warning">
                 Find Out How <span className="ml-2 right-icon">&#8594;</span>
               </a>
-            </div>
+            </div> */}
           </Col>
-          <Col lg={6}>
-            <div className="mt-5 mt-lg-0 hero-container">
-              <div className="backdrop"></div>
-              <img src="/images/logoRemake.png" alt="" className="img-fluid mx-auto d-block"/>
-             
-            </div>
+          <Col lg={4}>
+          <div className='hero-container'>
+            <div className='backdrop-hero'></div>
+            <img src="/images/landing-hero.png" alt="" className="img-fluid mx-auto d-block"/>
+
+          </div>
+          </Col>
+          <Col lg={4}>
           </Col>
         </Row>
       </Container>
